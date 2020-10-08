f1f54cbf98ad389f8e0d75cca8ea27d5715ec8cd
Sean LaFlam <sean.laflam@gmail.com>
2020-10-08 14:18:44 -0400

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+  fetch('http://localhost:3000/toys')
+  .then(function(response){
+    return response.json();
+  }).then(function(json) {
+    for (const toy in json) {
+      let div = document.createElement('div')
+      div.classList.add('card')
+        let h2 = document.createElement('h2')
+        h2.innerText = json[toy]['name']
+        div.append(h2)
+
+        let img = document.createElement('img')
+        img.src= json[toy]['image']
+        img.classList.add('toy-avatar')
+        div.append(img)
+
+        // let p = document.createElement('p')
+        // p.innerText = json[toy]['name']
+        // div.append(p)
+        //
+        // let button = document.createElement('button')
+        // button.innerText = json[toy]['name']
+        // div.append(button)
+
+      toyCollection = document.getElementById('toy-collection')
+      toyCollection.append(div)
+    }
+  })
+
+
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
